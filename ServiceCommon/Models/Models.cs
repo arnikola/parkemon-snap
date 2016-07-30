ff8e7e096b5482168d0af5bfcf9a97f828afe527
Reuben Bond <reuben.bond@outlook.com>
2016-07-30 14:39:25 +1000

    location distances

--- ServiceCommon/Models/Models.cs
+++ ServiceCommon/Models/Models.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace ServiceCommon.Models
 {
     using Orleans.Concurrency;
-    
+
     [Immutable]
     [Serializable]
     public class Report
@@ -16,7 +16,7 @@ namespace ServiceCommon.Models
         public Bounty Bounty { get; set; }
         public string Status { get; set; }
     }
-    
+
     [Immutable]
     [Serializable]
     public class Bounty
@@ -51,5 +51,28 @@ namespace ServiceCommon.Models
         public Location Location { get; set; }
         public string Name { get; set; }
         public Guid Id { get; set; }
+    }
+
+    public static class LocationUtility
+    {
+        public static double GetKilometersTo(Location one, Location other)
+        {
+            const double EarthRadiusInKilometers = 6371.0;
+            return EarthRadiusInKilometers*GetDistanceRatioTo(one, other);
+        }
+
+        public static double GetDistanceRatioTo(Location one, Location other)
+        {
+            const double DegreesToRadians = Math.PI/180;
+            var latRadiansA = one.Lat*DegreesToRadians;
+            var lonRadiansA = one.Long*DegreesToRadians;
+            var latRadiansB = other.Lat*DegreesToRadians;
+            var lonRadiansB = other.Long*DegreesToRadians;
+            var lonDelta = lonRadiansB - lonRadiansA;
+            var latDelta = latRadiansB - latRadiansA;
+            var d = Math.Pow(Math.Sin(latDelta/2.0), 2.0) +
+                    ((Math.Cos(latRadiansA)*Math.Cos(latRadiansB))*Math.Pow(Math.Sin(lonDelta/2.0), 2.0));
+            return 2.0*Math.Atan2(Math.Sqrt(d), Math.Sqrt(1.0 - d));
+        }
     }
 }
